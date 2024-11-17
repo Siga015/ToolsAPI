962ad4706ac6d4926d7db1a7f770f26ef302c888
Siga015 <118623034+Siga015@users.noreply.github.com>
2024-11-17 16:41:56 +0100

    Hotfix v3

---
+++ ToolsAPI/src/main/java/siga/toolsapi/item/version/MetaHandler.java
@@ -0,0 +1,12 @@
+package siga.toolsapi.item.version;
+
+import org.bukkit.inventory.meta.ItemMeta;
+
+public interface MetaHandler {
+
+    void setPersistentData(ItemMeta meta, String key, String value);
+
+    String getPersistentData(ItemMeta meta, String key);
+
+
+}
